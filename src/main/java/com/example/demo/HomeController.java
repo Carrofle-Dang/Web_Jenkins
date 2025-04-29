522b48b4fd8013185e296fb681523b97b32b77e1
Carrofle-Dang <45886715+Carrofle-Dang@users.noreply.github.com>
2025-04-29 15:13:30 +0900

    Update HomeController.java

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -4,9 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
+public class HomeController {   
     @GetMapping("/")
     public String home() {
         return "index"; // templates/index.html
-    }
+@GetMapping("/test")
+    public String b() {
+        return "B"; // templates/index.html
 }
